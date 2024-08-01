b78d5f2c98946f3aa32e9bc1ec670d7c8807a6a1
NISHIZUKA Yoshiki <116330146+YoshikiNishizuka@users.noreply.github.com>
2024-08-01 16:55:43 +0900

    PropertiesとFlyway追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.flywaydb:flyway-core:10.17.0")
+	implementation("org.flywaydb:flyway-database-postgresql:10.17.0")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
